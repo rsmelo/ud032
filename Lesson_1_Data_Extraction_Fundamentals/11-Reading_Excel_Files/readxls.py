e32adeccf702281d090bfdb1f23a392a7125a255
rsmelo <rodrigosm87@gmail.com>
2017-05-28 11:25:19 -0300

    solved 1-11 exercise

--- Lesson_1_Data_Extraction_Fundamentals/11-Reading_Excel_Files/readxls.py
+++ Lesson_1_Data_Extraction_Fundamentals/11-Reading_Excel_Files/readxls.py
@@ -2,16 +2,16 @@
 """
 Your task is as follows:
 - read the provided Excel file
-- find and return the min and max values for the COAST region
+- find and return the min, max and average values for the COAST region
 - find and return the time value for the min and max entries
 - the time values should be returned as Python tuples
 
 Please see the test function for the expected return format
 """
 
-import xlrd
 from zipfile import ZipFile
-datafile = "2013_ERCOT_Hourly_Load_Data.xls"
+import xlrd
+DATA_FILE = "2013_ERCOT_Hourly_Load_Data.xls"
 
 
 def open_zip(datafile):
@@ -23,43 +23,35 @@ def parse_file(datafile):
     workbook = xlrd.open_workbook(datafile)
     sheet = workbook.sheet_by_index(0)
 
-    ### example on how you can get the data
-    #sheet_data = [[sheet.cell_value(r, col) for col in range(sheet.ncols)] for r in range(sheet.nrows)]
+    data = [[sheet.cell_value(r, col) for col in range(sheet.ncols)]
+            for r in range(sheet.nrows)]
 
-    ### other useful methods:
-    # print "\nROWS, COLUMNS, and CELLS:"
-    # print "Number of rows in the sheet:", 
-    # print sheet.nrows
-    # print "Type of data in cell (row 3, col 2):", 
-    # print sheet.cell_type(3, 2)
-    # print "Value in cell (row 3, col 2):", 
-    # print sheet.cell_value(3, 2)
-    # print "Get a slice of values in column 3, from rows 1-3:"
-    # print sheet.col_values(3, start_rowx=1, end_rowx=4)
+    cv = sheet.col_values(1, start_rowx=1, end_rowx=None)
 
-    # print "\nDATES:"
-    # print "Type of data in cell (row 1, col 0):", 
-    # print sheet.cell_type(1, 0)
-    # exceltime = sheet.cell_value(1, 0)
-    # print "Time in Excel format:",
-    # print exceltime
-    # print "Convert time to a Python datetime tuple, from the Excel float:",
-    # print xlrd.xldate_as_tuple(exceltime, 0)
-    
-    
+    maxval = max(cv)
+    minval = min(cv)
+
+    maxpos = cv.index(maxval) + 1
+    minpos = cv.index(minval) + 1
+
+    maxtime = sheet.cell_value(maxpos, 0)
+    realtime = xlrd.xldate_as_tuple(maxtime, 0)
+    mintime = sheet.cell_value(minpos, 0)
+    realmintime = xlrd.xldate_as_tuple(mintime, 0)
+
     data = {
-            'maxtime': (0, 0, 0, 0, 0, 0),
-            'maxvalue': 0,
-            'mintime': (0, 0, 0, 0, 0, 0),
-            'minvalue': 0,
-            'avgcoast': 0
+        'maxtime': realtime,
+        'maxvalue': maxval,
+        'mintime': realmintime,
+        'minvalue': minval,
+        'avgcoast': sum(cv) / float(len(cv))
     }
     return data
 
 
 def test():
-    open_zip(datafile)
-    data = parse_file(datafile)
+    open_zip(DATA_FILE)
+    data = parse_file(DATA_FILE)
 
     assert data['maxtime'] == (2013, 8, 13, 17, 0, 0)
     assert round(data['maxvalue'], 10) == round(18779.02551, 10)
